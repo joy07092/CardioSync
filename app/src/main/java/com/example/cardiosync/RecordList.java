ae4fab4d129db2ddeddec648e69f31541654eec1
imtiaj_joy <imtiagjoy@gmail.com>
2023-06-16 21:28:33 +0600

    javadoc added

--- app/src/main/java/com/example/cardiosync/RecordList.java
+++ app/src/main/java/com/example/cardiosync/RecordList.java
@@ -2,17 +2,33 @@ package com.example.cardiosync;
 
 import java.util.ArrayList;
 
-public class RecordList {   //list of records
+/**
+ * This is class for manipulation of data in the list
+ */
+public class RecordList {
     public static ArrayList<ModelClass> mcl= new ArrayList<>();
 
-    public void addRecord(ModelClass modelClass){  //method to add new record
+    /**
+     * This is the method for adding new record in the list
+     *
+     * @param modelClass
+     *              this is record that will be added in the list
+     */
+    public void addRecord(ModelClass modelClass){
         if(mcl.contains(modelClass)){
             throw new IllegalArgumentException();
         }
         mcl.add(modelClass);
     }
 
-    public void deleteRecord(int position){  //method to delete record
+    /**
+     * This is the method for deleting record from the list
+     *
+     * @param position
+     *              this is position of the record that will be deleted from the list
+     */
+
+    public void deleteRecord(int position){
         if (position>=0 && position<mcl.size()) {
             mcl.remove(position);
         }
@@ -21,7 +37,12 @@ public class RecordList {   //list of records
         }
     }
 
+    /**
+     * This method is for returning the size of the list
+     * @return
+     *      Return the size of list of record
+     */
     public int  count(){
         return  mcl.size();
-    }   //method to count the number of total records
+    }
 }
--- app/src/main/java/com/example/cardiosync/Utils.java
+++ app/src/main/java/com/example/cardiosync/Utils.java
@@ -5,8 +5,17 @@ import android.content.SharedPreferences;
 
 import com.google.gson.Gson;
 
+/**
+ * This is class used for the purpose of UI Testing
+ */
 public class Utils {
-    public  void saveData(Context context)  //used in the ui test
+
+    /**
+     * This is method is for converting information in JSON format and storing them
+     * @param context
+     *               The context from which the data has been input
+     */
+    public  void saveData(Context context)
     {
         SharedPreferences sharedPreferences = context.getSharedPreferences("project",context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
