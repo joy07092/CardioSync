27ae7d51d885176972f21e481cb96b27db2fe60e
imtiaj_joy <imtiagjoy@gmail.com>
2023-06-16 16:34:46 +0600

    Main Activity first update and Data entry activity added

--- app/src/main/java/com/example/cardiosync/MainActivity.java
+++ app/src/main/java/com/example/cardiosync/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.cardiosync;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView1;
+    public static TaskAdapter adapter;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    ModelClass modelclass;
+    Button button;
+    String s1,s2,s3,s4;
+    Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.AddBUttonId);
+
+        retrieveData();  //method to retrieve data using sharedpreference
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,DataEntry.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void retrieveData() {  //for getting the stored data
+
     }
 }
